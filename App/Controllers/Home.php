e00aa2dcff878bac20539ef9f52168efd40546b4
duypham000 <khanhduy.dev@gmail.com>
2022-10-26 22:38:19 +0700

    update slider

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -20,7 +20,34 @@ class Home extends \Core\Controller
      */
     public function indexAction()
     {
-        View::renderTemplate('Home/index.html', []);
+        View::renderTemplate('Home/index.html', [
+            'status' => 'OK',
+            'data' => [
+                'newBook' => [
+                    [
+                        "title" => "Hành tinh song song",
+                        "href" => "#",
+                        "thumble" => "/assets/images/book-sample.jpg",
+                    ], [
+                        "title" => "Hành tinh cho kẻ nghĩ nhiều",
+                        "href" => "#",
+                        "thumble" => "/assets/images/book-sample.jpg",
+                    ], [
+                        "title" => "Hiệu ứng chim mồi",
+                        "href" => "#",
+                        "thumble" => "/assets/images/book-sample.jpg",
+                    ], [
+                        "title" => "Bạn chẳng thông minh lắm đâu",
+                        "href" => "#",
+                        "thumble" => "/assets/images/book-sample.jpg",
+                    ], [
+                        "title" => "Thiên nga đen",
+                        "href" => "#",
+                        "thumble" => "/assets/images/book-sample.jpg",
+                    ],
+                ]
+            ]
+        ]);
     }
 
     /**
@@ -33,7 +60,31 @@ class Home extends \Core\Controller
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo json_encode([
                 'status' => 'OK',
-                'data' => []
+                'data' => [
+                    'sampleBook' => [
+                        [
+                            "title" => "Hành tinh song song",
+                            "href" => "#",
+                            "thumble" => "/assets/images/book-sample.jpg",
+                        ], [
+                            "title" => "Hành tinh cho kẻ nghĩ nhiều",
+                            "href" => "#",
+                            "thumble" => "/assets/images/book-sample.jpg",
+                        ], [
+                            "title" => "Hiệu ứng chim mồi",
+                            "href" => "#",
+                            "thumble" => "/assets/images/book-sample.jpg",
+                        ], [
+                            "title" => "Bạn chẳng thông minh lắm đâu",
+                            "href" => "#",
+                            "thumble" => "/assets/images/book-sample.jpg",
+                        ], [
+                            "title" => "Thiên nga đen",
+                            "href" => "#",
+                            "thumble" => "/assets/images/book-sample.jpg",
+                        ],
+                    ]
+                ]
             ]);
         }
     }
---
+++ public/assets/js/home.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+var swiper = new Swiper(".mySwiper", {
+  Infinity: 1,
+});
