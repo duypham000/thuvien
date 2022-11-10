4723b9ae5c1df0645da0c64967067771080bf6a9
duypham000 <khanhduy.dev@gmail.com>
2022-11-10 07:06:05 +0700

    wtf update

--- App/Controllers/Book.php
+++ App/Controllers/Book.php
@@ -52,22 +52,32 @@ class Book extends \Core\Controller
           [
             "title" => "Hành tinh song song",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ], [
             "title" => "Hành tinh cho kẻ nghĩ nhiều",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ], [
             "title" => "Hiệu ứng chim mồi",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ], [
             "title" => "Bạn chẳng thông minh lắm đâu",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ], [
             "title" => "Thiên nga đen",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ],
         ],
--- App/Controllers/ListBook.php
+++ App/Controllers/ListBook.php
@@ -26,27 +26,38 @@ class ListBook extends \Core\Controller
         ["label" => "danh sách", "url" => "#"],
         ["label" => "tác giả", "url" => ""]
       ],
+      'title' => "Danh sách demo",
       'data' => [
         'books' => [
           [
             "title" => "Hành tinh song song",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ], [
             "title" => "Hành tinh cho kẻ nghĩ nhiều",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ], [
             "title" => "Hiệu ứng chim mồi",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ], [
             "title" => "Bạn chẳng thông minh lắm đâu",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ], [
             "title" => "Thiên nga đen",
             "href" => "/sach/15",
+            "author" => "Duyn",
+            "price" => "125,000 đ",
             "thumble" => "/assets/images/book-sample.jpg",
           ],
         ],
